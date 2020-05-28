a82ac978ca832b1d542c5ccfd79d3e1ee188cf2d
Sofia Pacifico <pacifico.sofi@gmail.com>
2020-05-28 10:50:04 -0500

    first commit staging 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   template: `<div>Hello {{value}}</div>`,
 })
 export class AppComponent {
-  value = 'Static app staging enviroment';
+  value = 'Static app staging enviroment true';
 }
